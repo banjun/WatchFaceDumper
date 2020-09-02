0f119f2bf10b710d86aaa73c619eb86f95860d2f
banjun <banjun@gmail.com>
2020-09-02 23:28:03 +0900

    Initial Commit

---
+++ WatchFaceDumper/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  WatchFaceDumper
+//
+//  Created by BAN Jun on 2020/09/02.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WatchFaceDumper/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  WatchFaceDumper
+//
+//  Created by BAN Jun on 2020/09/02.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
