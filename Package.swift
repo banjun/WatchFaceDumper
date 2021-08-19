3106b7f34ab2e721e2daf2493bfbd642456ccc8d
banjun <banjun@gmail.com>
2021-08-19 21:22:00 +0900

    update swift-mod for Swift 5.4

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         // for generating public memberwise init by `swift run -c release swift-mod`
-        .package(url: "https://github.com/ra1028/swift-mod.git", from: "0.0.4")
+        .package(url: "https://github.com/ra1028/swift-mod.git", .revision("0.0.6")), // use revision instead of `from` to allow unstable dependency ref from swift-mod to swift-syntax
     ],
     targets: [
         .target(
